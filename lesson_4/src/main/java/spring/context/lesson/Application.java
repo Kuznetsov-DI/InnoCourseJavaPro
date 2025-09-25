2e40d42e9e289200cc617bddfbaf748f53dbf3bb
Dmitry Kuznetcov <kuznetcov.rabota@mail.ru>
2025-09-25 15:11:02 +0300

    исправил замечания

--- lesson_4/src/main/java/spring/context/lesson/Application.java
+++ lesson_4/src/main/java/spring/context/lesson/Application.java
@@ -15,24 +15,24 @@ public class Application {
 
         UserService service = context.getBean(UserServiceImpl.class);
 
-//        var users = service.getAllUsers();
-//        System.out.println(users);
-//
-//        var user = service.getUser(1L);
-//        System.out.println(user);
+        var users = service.getAllUsers();
+        System.out.println(users);
 
-//        var userForCreating = new User();
-//        userForCreating.setUsername("ignat");
-//        var createdId = service.createUser(userForCreating);
-//        System.out.println(createdId);
-//        var createdUser = service.getUser(createdId);
-//        System.out.println(createdUser);
+        var user = service.getUser(1L);
+        System.out.println(user);
 
-//        var userForUpdating = new User();
-//        userForUpdating.setUsername("fedor");
-//        var updatedId = service.updateUser(1L, userForUpdating);
-//        var updatedUser = service.getUser(updatedId);
-//        System.out.println(updatedUser);
+        var userForCreating = new User();
+        userForCreating.setUsername("ignat");
+        var createdId = service.createUser(userForCreating);
+        System.out.println(createdId);
+        var createdUser = service.getUser(createdId);
+        System.out.println(createdUser);
+
+        var userForUpdating = new User();
+        userForUpdating.setUsername("fedor");
+        var updatedId = service.updateUser(1L, userForUpdating);
+        var updatedUser = service.getUser(updatedId);
+        System.out.println(updatedUser);
 
         service.deleteUser(1L);
         var deletedUser = service.getUser(1L);
--- lesson_4/src/main/java/spring/context/lesson/database/configuration/DataBaseConfig.java
+++ lesson_4/src/main/java/spring/context/lesson/database/configuration/DataBaseConfig.java
@@ -3,21 +3,28 @@ package spring.context.lesson.database.configuration;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:application.properties")
 public class DataBaseConfig {
 
     @Getter
     private final HikariDataSource dataSource;
 
-    public DataBaseConfig() {
+    public DataBaseConfig(@Value("${db.databaseUrl}") String databaseUrl,
+                          @Value("${db.username}") String username,
+                          @Value("${db.password}") String password,
+                          @Value("${db.diver}") String diver,
+                          @Value("${db.maxPoolSize}") int maxPoolSize) {
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:postgresql://localhost:5432/java_pro_course");
-        config.setUsername("root");
-        config.setPassword("123456");
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setMaximumPoolSize(10);
+        config.setJdbcUrl(databaseUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setDriverClassName(diver);
+        config.setMaximumPoolSize(maxPoolSize);
 
         this.dataSource = new HikariDataSource(config);
     }
